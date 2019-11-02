47806217f511efc0e8c74a0d945dc2d0f9ea5204
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2019-11-02 12:18:23 +0700

    Craete feedback models and setup websocket

---
+++ app/Http/Controllers/FeedbackController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Feedback;
+
+class FeedbackController extends Controller
+{
+    public function store(Request $request)
+    {
+        $words = $request('words');
+
+        if (strpos($words, ',') != false) {
+            return response()->json([
+                'message' => 'Kata tidak ada boleh tanda koma (,)'
+            ], 400);
+        }
+
+        $words = explode(" ", $words);
+        if (count($word) > 3) {
+            return response()->json([
+                'message' => 'Tidak boleh lebih dari 3 kata'
+            ], 400);
+        }
+
+        foreach ($words as $key => $word) {
+            $this->createOrIncrement($word);
+        }
+
+        return response()->json('ok');
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function dashboardData()
+    {
+        return response()->json($this->getData());
+    }
+
+    public function getData()
+    {
+        $topTen = Feedback::orderBy('count', 'DESC')
+            ->get()
+            ->take(10);
+
+        return $topTen;
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function createOrIncrement()
+    {
+        $word = strtolower($word);
+
+        $feedback = Feedback::where('word', $word)->first();
+
+        if ($feedback) {
+            $feedback->increment('count');
+        } else {
+            Feedback::create([
+                'word' => $word,
+                'count' => 1
+            ]);
+        }
+    }
+}
